8e44120c35b12ca4e4214ae4199acf41351bec20
Nabila Ayaba <nabilaayaba@gmail.com>
2025-01-15 11:12:38 +0300

    Final lab

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector("#dark-mode-toggle")
-
-toggleButton.addEventListener("click", () => {
-    const isDarkMode = document.body.classList.toggle("dark-mode");
-    toggleButton.textContent = isDarkMode ? "Light Mode 🌞" : "Dark Mode 🌓";
-  })
